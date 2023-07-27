c08f42a20fe0f72d2afb7ae0eef02f6a59b106e9
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-07-27 18:32:15 +0530

    Added HiddenAppBar

--- lib/Components/app_bar.dart
+++ lib/Components/app_bar.dart
@@ -1,9 +1,23 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
-// ignore: non_constant_identifier_names
 AppBar HiddenAppBar() {
   return AppBar(
     backgroundColor: Colors.transparent,
     elevation: 0,
+    leading: Builder(
+      builder: (context) {
+        return IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        );
+      },
+    ),
   );
 }
