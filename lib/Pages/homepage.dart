90e72e45fe1bf7d146838e6389b2e160942dab01
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-07-27 18:32:41 +0530

    Added Home Screen; Structure Only

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,6 +1,9 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:book_club/Components/app_bar.dart';
+import 'package:book_club/Components/title_text.dart';
+import 'package:book_club/Models/theme.dart';
+import 'package:book_club/Painters/curve_painter.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,13 +15,34 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Home Screen',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ),
+    return Consumer(
+      builder: (context, ThemeModel value, child) {
+        return Scaffold(
+            appBar: HiddenAppBar(),
+            extendBodyBehindAppBar: true,
+            body: SingleChildScrollView(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: CustomPaint(
+                  painter: CurvePainter(),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.12,
+                          ),
+                          child: TitleText(context: context),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ));
+      },
     );
   }
 }
