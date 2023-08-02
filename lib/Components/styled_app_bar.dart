34b766c688cbc374f59be40954179fc42ab69c3c
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-08-03 00:54:01 +0530

    Styled App bar

---
+++ lib/Components/styled_app_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class StyledAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const StyledAppBar({
+    super.key,
+    this.preferredSize = const Size.fromHeight(50),
+    required this.buttontitle,
+    required this.buttonFunction,
+    required this.title,
+  });
+
+  @override
+  final Size preferredSize;
+
+  final String title;
+  final String buttontitle;
+  final VoidCallback buttonFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.primary,
+          fontSize: 20,
+          fontFamily: "Lugrasimo",
+        ),
+      ),
+      leading: Builder(
+        builder: (context) {
+          return IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_new_rounded,
+              size: 25,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            onPressed: () {
+              Navigator.pop(context, );
+            },
+          );
+        },
+      ),
+      actions: [
+        TextButton(
+          onPressed: buttonFunction,
+          child: Text(
+            buttontitle,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
